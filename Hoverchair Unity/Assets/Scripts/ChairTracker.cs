6898ef9ce93c1ec8d945ce74a1561418ac3d733a
Eric Smith <motionsmith@gmail.com>
2016-03-16 10:36:27 -0700

    Initial checkin

---
+++ Hoverchair Unity/Assets/Scripts/ChairTracker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ChairTracker : MonoBehaviour {
+
+    public enum ChairTrackMode
+    {
+        NotAChair,
+        AChair
+    }
+
+    public ChairTrackMode chairTrackMode
+    {
+        get; private set;
+    }
+
+    public GameObject chairGameObject;
+
+    SteamVR_TrackedObject trackedObj;
+
+    // Use this for initialization
+    void Awake()
+    {
+        chairTrackMode = ChairTrackMode.NotAChair;
+
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            chairTrackMode = (chairTrackMode == ChairTrackMode.AChair) ? ChairTrackMode.NotAChair : ChairTrackMode.AChair;
+            chairGameObject.SetActive(chairTrackMode == ChairTrackMode.AChair);
+        }
+	}
+}
---
+++ Hoverchair Unity/Assets/Scripts/MimicAngle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MimicAngle : MonoBehaviour {
+
+	[System.Serializable]
+	public enum RotationAxis {
+		x,
+		y,
+		z
+	}
+
+	public Transform sourceXform;
+	public RotationAxis sourceAxis;
+	public RotationAxis destinationAxis;
+	public float rotationalOffset = 0;
+	public float minimumAngle = 0;
+	public float maximumAngle = 360;
+
+	Transform destinationXform;
+
+	void Awake () {
+		destinationXform = GetComponent<Transform> ();
+	}
+
+	void Update () {
+        if (sourceXform == null) {
+            return;
+        }
+
+		Vector3 newRotation = destinationXform.localEulerAngles;
+
+		float sourceAxisValue = sourceXform.localEulerAngles.x;
+		if (sourceAxis == RotationAxis.y) {
+			sourceAxisValue = sourceXform.localEulerAngles.y;
+		} else if (sourceAxis == RotationAxis.z) {
+			sourceAxisValue = sourceXform.localEulerAngles.z;
+		}
+
+		if (destinationAxis == RotationAxis.x) {
+			newRotation.x = Mathf.Clamp((sourceAxisValue + rotationalOffset) % 360, minimumAngle, maximumAngle);
+			Debug.Log (newRotation);
+		} else if (destinationAxis == RotationAxis.y) {
+			newRotation.y = sourceAxisValue;
+		} else {
+			newRotation.z = sourceAxisValue;
+		}
+
+		destinationXform.localEulerAngles = newRotation;
+	}
+}
